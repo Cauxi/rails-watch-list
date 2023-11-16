eebead2933768c416c8aa67e1c34f3a47fff4b73
Carlos Coelho <carlos.neto.coelho@outlook.pt>
2023-11-16 12:34:53 +0000

    created routes and controllers, and also started to work on view

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,31 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create delete]
+
+  def new
+    @bookmark = bookmark.new
+  end
+
+  def create
+    @bookmark = bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    @bookmark.destroy
+  end
+
+  private
+
+  def set_list
+    @list = list.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
